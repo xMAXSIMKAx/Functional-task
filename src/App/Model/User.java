36c68b1749e6cf128d13e7db8d3317a465c4d25b
xMAXSIMKAx <maxymram@gmail.com>
2024-02-21 18:30:46 +0100

    Initial commit add model

---
+++ src/App/Model/User.java
@@ -0,0 +1,18 @@
+package App.Model;
+
+public class User {
+    private String name;
+    private String phone;
+
+    public User(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getPhone() {
+        return phone;
+    }
+}
